8dfb0feb7c78f3515700a7f9404ba1d7b2cb0583
RusselxD <russelcabigquez8@gmail.com>
2026-04-05 14:35:31 +0800

    added some entries in techStackData

--- src/data/techStackData.ts
+++ src/data/techStackData.ts
@@ -1,211 +1,235 @@
 export interface TechStack {
-    name: string;
-    icon: string;
-    color?: string;
+  name: string;
+  icon: string;
+  color?: string;
 }
 
 export interface SkillGroup {
-    title: string;
-    technologies: TechStack[];
+  title: string;
+  technologies: TechStack[];
 }
 
 export const techStackData: SkillGroup[] = [
-    {
-        title: "Languages",
-        technologies: [
-            {
-                name: "JavaScript",
-                icon: "logos:javascript",
-            },
-            {
-                name: "TypeScript",
-                icon: "logos:typescript-icon",
-            },
-            {
-                name: "Python",
-                icon: "logos:python",
-            },
-            {
-                name: "Java",
-                icon: "logos:java",
-            },
-            {
-                name: "C#",
-                icon: "devicon:csharp",
-            },
-            {
-                name: "C",
-                icon: "skill-icons:c",
-            },
-            {
-                name: "PHP",
-                icon: "logos:php",
-            },
-        ],
-    },
-    {
-        title: "Databases",
-        technologies: [
-            {
-                name: "MySQL",
-                icon: "logos:mysql",
-            },
-            {
-                name: "PostgreSQL",
-                icon: "logos:postgresql",
-            },
-            {
-                name: "MongoDB",
-                icon: "logos:mongodb-icon",
-            },
-            {
-                name: "SQL Server",
-                icon: "devicon:microsoftsqlserver",
-            },
-            {
-                name: "SQL Alchemy",
-                icon: "devicon-plain:sqlalchemy",
-            },
-        ],
-    },
-    {
-        title: "Cloud & DevOps",
-        technologies: [
-            {
-                name: "Vercel",
-                icon: "logos:vercel-icon",
-            },
-            {
-                name: "Render",
-                icon: "simple-icons:render",
-            },
-        ],
-    },
-    {
-        title: "Frameworks & Libraries",
-        technologies: [
-            {
-                name: "React",
-                icon: "logos:react",
-            },
-            {
-                name: "Node.js",
-                icon: "logos:nodejs-icon",
-            },
-            {
-                name: "Express.js",
-                icon: "skill-icons:expressjs-light",
-            },
-            {
-                name: ".NET",
-                icon: "logos:dotnet",
-            },
-            {
-                name: "FastAPI",
-                icon: "devicon:fastapi",
-            },
-            {
-                name: "Livewire",
-                icon: "devicon:livewire",
-            },
-            {
-                name: "Laravel",
-                icon: "logos:laravel",
-            },
-            {
-                name: "Alpine JS",
-                icon: "devicon:alpinejs",
-            },
-            {
-                name: "Axios",
-                icon: "simple-icons:axios",
-            },
-            {
-                name: "JWT",
-                icon: "devicon:jwt",
-            },
-            {
-                name: "Spring Boot",
-                icon: "logos:spring-icon",
-            },
-            {
-                name: "Tailwind CSS",
-                icon: "logos:tailwindcss-icon",
-            },
-            {
-                name: "daisyUI",
-                icon: "simple-icons:daisyui",
-                color: "#5A0EF8",
-            },
-        ],
-    },
+  {
+    title: "Languages",
+    technologies: [
+      {
+        name: "JavaScript",
+        icon: "logos:javascript",
+      },
+      {
+        name: "TypeScript",
+        icon: "logos:typescript-icon",
+      },
+      {
+        name: "Python",
+        icon: "logos:python",
+      },
+      {
+        name: "Java",
+        icon: "logos:java",
+      },
+      {
+        name: "C#",
+        icon: "devicon:csharp",
+      },
+      {
+        name: "C",
+        icon: "skill-icons:c",
+      },
+      {
+        name: "PHP",
+        icon: "logos:php",
+      },
+    ],
+  },
+  {
+    title: "Databases",
+    technologies: [
+      {
+        name: "MySQL",
+        icon: "logos:mysql",
+      },
+      {
+        name: "PostgreSQL",
+        icon: "logos:postgresql",
+      },
+      {
+        name: "MongoDB",
+        icon: "logos:mongodb-icon",
+      },
+      {
+        name: "SQL Server",
+        icon: "devicon:microsoftsqlserver",
+      },
+      {
+        name: "SQL Alchemy",
+        icon: "devicon-plain:sqlalchemy",
+      },
+    ],
+  },
+  {
+    title: "AI & Machine Learning",
+    technologies: [
+      {
+        name: "OpenCV",
+        icon: "logos:opencv",
+      },
+      {
+        name: "Ultralytics",
+        icon: "simple-icons:ultralytics",
+        color: "#0060FF",
+      },
+      {
+        name: "Roboflow",
+        icon: "simple-icons:roboflow",
+        color: "#A351FB",
+      },
+    ],
+  },
+  {
+    title: "Frameworks & Libraries",
+    technologies: [
+      {
+        name: "React",
+        icon: "logos:react",
+      },
+      {
+        name: "Node.js",
+        icon: "logos:nodejs-icon",
+      },
+      {
+        name: "Express.js",
+        icon: "skill-icons:expressjs-light",
+      },
+      {
+        name: ".NET",
+        icon: "logos:dotnet",
+      },
+      {
+        name: "FastAPI",
+        icon: "devicon:fastapi",
+      },
+      {
+        name: "Livewire",
+        icon: "devicon:livewire",
+      },
+      {
+        name: "Laravel",
+        icon: "logos:laravel",
+      },
+      {
+        name: "Alpine JS",
+        icon: "devicon:alpinejs",
+      },
+      {
+        name: "Axios",
+        icon: "simple-icons:axios",
+      },
+      {
+        name: "JWT",
+        icon: "devicon:jwt",
+      },
+      {
+        name: "Spring Boot",
+        icon: "logos:spring-icon",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "logos:tailwindcss-icon",
+      },
+      {
+        name: "daisyUI",
+        icon: "simple-icons:daisyui",
+        color: "#5A0EF8",
+      },
+    ],
+  },
 
-    {
-        title: "Tools & Platforms",
-        technologies: [
-            {
-                name: "Git",
-                icon: "logos:git-icon",
-            },
-            {
-                name: "Github",
-                icon: "akar-icons:github-fill",
-            },
-            {
-                name: "VS Code",
-                icon: "logos:visual-studio-code",
-            },
-            {
-                name: "Visual Studio",
-                icon: "logos:visual-studio",
-            },
-            {
-                name: "PyCharm",
-                icon: "logos:pycharm",
-            },
-            {
-                name: "Postman",
-                icon: "logos:postman-icon",
-            },
-            {
-                name: "Ubuntu",
-                icon: "logos:ubuntu",
-            },
-            {
-                name: "NPM",
-                icon: "logos:npm-icon",
-            },
-            {
-                name: "Swagger",
-                icon: "logos:swagger",
-            },
-            {
-                name: "Vite",
-                icon: "logos:vitejs",
-            },
-            {
-                name: "Figma",
-                icon: "logos:figma",
-            },
-        ],
-    },
-    {
-        title: "AI & Machine Learning",
-        technologies: [
-            {
-                name: "OpenCV",
-                icon: "logos:opencv",
-            },
-            {
-                name: "Ultralytics",
-                icon: "simple-icons:ultralytics",
-                color: "#0060FF",
-            },
-            {
-                name: "Roboflow",
-                icon: "simple-icons:roboflow",
-                color: "#A351FB",
-            },
-        ],
-    },
+  {
+    title: "Tools & Platforms",
+    technologies: [
+      {
+        name: "Git",
+        icon: "logos:git-icon",
+      },
+      {
+        name: "Github",
+        icon: "akar-icons:github-fill",
+      },
+      {
+        name: "VS Code",
+        icon: "logos:visual-studio-code",
+      },
+      {
+        name: "Visual Studio",
+        icon: "logos:visual-studio",
+      },
+      {
+        name: "PyCharm",
+        icon: "logos:pycharm",
+      },
+      {
+        name: "IntelliJ IDEA",
+        icon: "logos:intellij-idea",
+      },
+      {
+        name: "Postman",
+        icon: "logos:postman-icon",
+      },
+      {
+        name: "Sentry",
+        icon: "simple-icons:sentry",
+      },
+      {
+        name: "Ubuntu",
+        icon: "logos:ubuntu",
+      },
+      {
+        name: "NPM",
+        icon: "logos:npm-icon",
+      },
+      {
+        name: "Swagger",
+        icon: "logos:swagger",
+      },
+      {
+        name: "Vite",
+        icon: "logos:vitejs",
+      },
+      {
+        name: "Figma",
+        icon: "logos:figma",
+      },
+    ],
+  },
+  {
+    title: "Cloud & DevOps",
+    technologies: [
+      {
+        name: "Vercel",
+        icon: "logos:vercel-icon",
+      },
+      {
+        name: "Render",
+        icon: "simple-icons:render",
+      },
+      {
+        name: "Cloudflare",
+        icon: "logos:cloudflare-icon",
+      },
+      {
+        name: "Railway",
+        icon: "simple-icons:railway",
+      },
+      {
+        name: "Docker",
+        icon: "logos:docker-icon",
+      },
+      {
+        name: "Cloudinary",
+        icon: "simple-icons:cloudinary",
+      },
+    ],
+  },
 ];
